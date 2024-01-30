e2e4d72f1d7657f75bcd15c2d53d6cc89e166837
parthdasawant <parthdasawant07@gmail.com>
2024-01-30 23:02:21 +0530

    cleaning

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:rapidd_todos/views/todo_list.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../viewmodels/todo_viewmodel.dart';
